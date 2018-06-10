e2892e8a6f97be8721e5c34f274586d314ede2d5
agaayush <agaayush@amazon.com>
2018-06-10 12:03:18 +0530

    Fixed Email Modeling Errors

--- lambda/src/main/java/com/models/EmailModel.java
+++ lambda/src/main/java/com/models/EmailModel.java
@@ -2,6 +2,7 @@ package main.java.com.models;
 
 import lombok.Data;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
@@ -9,10 +10,14 @@ public class EmailModel {
 
     Map<String, EmailTranscriptDetails> userData;
 
+    public EmailModel() {
+        userData = new HashMap<String, EmailTranscriptDetails>();
+    }
+
     public void add(String userId, TranscriptionObject transcriptionObject) {
         String transcript = transcriptionObject.getResults().getTranscripts().get(0).getTranscript();
         String creationTime = transcriptionObject.getCreationTime();
-        String completionTime = transcriptionObject.getCreationTime();
+        String completionTime = transcriptionObject.getCompletionTime();
         EmailTranscriptDetails emailTranscriptDetails = new EmailTranscriptDetails();
         emailTranscriptDetails.setTranscript(transcript);
         emailTranscriptDetails.setCreationTime(creationTime);
